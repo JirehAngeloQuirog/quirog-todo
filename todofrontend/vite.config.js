afac5b72b6460a01f5622502b722da26b7c4cf33
Jireh Angelo S. Quirog <quirogybo21@gmail.com>
2025-04-07 16:39:45 +0800

    adjusted

--- todofrontend/vite.config.js
+++ todofrontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quirog-todo/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
